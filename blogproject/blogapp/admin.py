bb352f944cb04e5b9a702ac289fdc9bad72667d4
Shahin <shahinalisheikh@gmail.com>
2019-10-02 21:57:48 +0600

    django models+Admin is ready

--- blogproject/blogapp/admin.py
+++ blogproject/blogapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . models import Category,Author,Post 
+
+
+
 
 # Register your models here.
+class post(admin.ModelAdmin):
+    list_display=["__str__","author","category"]
+    class meta:
+        model=Post
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Post,post)
--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# creating a post author
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
+
+# creating a post category
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# create a post for blog
+class Post(models.Model):
+    title=models.CharField(max_length=500)
+    description=models.TextField(blank=False)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
