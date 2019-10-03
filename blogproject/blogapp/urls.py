dd4d5101e8822fb2e6fac24ec8dac54cc45de0da
Shahin <shahinalisheikh@gmail.com>
2019-10-03 12:04:36 +0600

    archive and category page extended from base.html////

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns=[
 
     path('',views.index,name='index'),
+    path('category',views.category,name='category'),
+    path('archive',views.archive,name='archive'),
+    path('contact',views.contact,name='contact'),
+    
 
 
 ]
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'blogapp/index.html')
+
+
+
+
+def category(request):
+    return render(request,'blogapp/category.html')
+
+
+
+
+def archive(request):
+    return render(request,'blogapp/archive.html')
+
+
+
+
+
+def contact(request):
+    return render(request,'blogapp/contact.html')
